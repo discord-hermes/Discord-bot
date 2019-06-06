39df1b199873f27f3ae27cdc1ad3ab8959371f98
Hayangggames <45326612+hayanggames@users.noreply.github.com>
2019-06-06 17:55:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 
 app = discord.Client()
 ##test token
-token = "NTYyODkzNDA3MDIyODc0NjU0.XKRZXQ.TbDAucqRpmN36RqVkny8WLOS3ZE"
+token = "NjfsdiIfdsldafjiLidfvjleir20gfisvcfaskdfldf94fjlkxjapok"
 
 @app.event
 async def on_ready():
@@ -21,14 +21,5 @@ async def on_message(message):
     if message.author.bot:
         return None
 
-    if message.content == "!도움":
-        await app.send_message(message.channel, "!도움\n어찌구\n저찌구\n쏼라\n쏼라")
-    
-    elif message.content == "!정보":
-        await app.send_message(message.channel, "MercuryBot Server Version :\nv0.1\n\nMercury Bot은 Team Hermes(팀 헤르메스)가 개발한\ndiscord.py 봇입니다.")
-
-    elif message.content == "!며칠":
-        now = datetime.datetime.now()
-        await app.send_message(message.channel, str(now.day)+"일임."]))
 
 app.run(token)
